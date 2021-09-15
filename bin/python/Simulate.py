146284e668f516f31b4e7e00be1a76eb341ce034
Mahsa Arabi <marabi@umass.edu>
2021-09-15 18:48:48 -0400

    Modifying simulation

--- bin/python/Simulate.py
+++ bin/python/Simulate.py
@@ -24,7 +24,8 @@ def run_cost_simulation_statusQuo_strategy(years_of_analysis):
     df=pd.DataFrame()
     for t in range (years_of_analysis):
         for i in range (len(line_segment_array)):
-            line_segment_array[i].update_age_and_underground_statusQuo()
+            line_segment_array[i].update_underground_status()
+            line_segment_array[i].update_age()
             line_segment_array[i].add_replcost_intrest_rate()
             line_segment_array[i].calculate_capex()
             line_segment_array[i].calculate_opex()
@@ -56,26 +57,22 @@ def run_cost_simulation_statusQuo_strategy(years_of_analysis):
 def run_cost_simulation_under_after_lifespan_strategy(years_of_analysis):
     line_segment_array=[]
     line_segment_length_array=[]
-    line_segment_age_array=[]
-    line_segment_underground_array=[]
-    line_segment_underground_length_total_array=[0 for t in range (years_of_analysis)]
     for i in range (Network.segment_number):
         segment=Network.Line_segment(Network.age_shape, Network.age_scale, Network.length_shape, Network.length_scale, 0.66)
         line_segment_array.append(segment)
         line_segment_length_array.append(segment.length)
-        line_segment_age_array.append(segment.age[0])
-        line_segment_underground_array.append(segment.underground[0])
-        line_segment_underground_length_total_array[0]+=line_segment_array[i].underground[0]
     np.random.seed(10101)
     random.seed(10102)
-    #df_line_segment_array=pd.DataFrame([line_segment_length_array, line_segment_age_array,line_segment_underground_array]).transpose()
-    #df_line_segment_array.columns=['length','base year age','base year underground']
-    #underground_length_new=[]
     df=pd.DataFrame()
-    #environmental_cost=[]
     for t in range (years_of_analysis):
         for i in range (len(line_segment_array)):
-            line_segment_array[i].update_age_and_underground_under_after_lifespan()
+            convert_new=False
+            lifespan_exceeded=line_segment_array[i].update_age()
+            if lifespan_exceeded==True:
+                convert_new+=True
+            else:
+                pass
+            line_segment_array[i].update_underground_status(convert=convert_new)
             line_segment_array[i].add_replcost_intrest_rate()
             line_segment_array[i].calculate_capex()
             line_segment_array[i].calculate_opex()
@@ -85,32 +82,7 @@ def run_cost_simulation_under_after_lifespan_strategy(years_of_analysis):
             line_segment_array[i].calculate_non_fatal_cost()
             line_segment_array[i].calculate_fatal_cost()
             line_segment_array[i].calculate_total_safety()
-            line_segment_array[i].calculate_total_cost()
-            #if t==0:
-            #    environmental_cost=0
-            #    safety_rate=1
-            #    non_fatal_safety_cost= safety_rate * line_segment_array[i].calculate_non_fatal_cost()
-            #    fatal_safety_cost= safety_rate * line_segment_array[i].calculate_fatal_cost()
-            #    total_safety_cost= safety_rate * line_segment_array[i].calculate_total_safety()
-            #else: 
-            #    if line_segment_array[i].underground[t]==line_segment_array[i].underground[t-1]:
-            #       environmental_cost=0
-            #       safety_rate=1
-            #        non_fatal_safety_cost= safety_rate * line_segment_array[i].calculate_non_fatal_cost()
-            #        fatal_safety_cost= safety_rate * line_segment_array[i].calculate_fatal_cost()
-            #        total_safety_cost= safety_rate * line_segment_array[i].calculate_total_safety()
-            #    else:
-            #        environmental_cost=line_segment_array[i].calculate_environmental_restoration()
-            #       safety_rate=(1+line_segment_array[i].length)
-            #       non_fatal_safety_cost= safety_rate * line_segment_array[i].calculate_non_fatal_cost()
-            #       fatal_safety_cost= safety_rate * line_segment_array[i].calculate_fatal_cost()
-            #       total_safety_cost= safety_rate * line_segment_array[i].calculate_total_safety()
-                    
-                
-            #line_segment_array[i].calculate_environmental_restoration()
-            #line_segment_capex_array[t]+=line_segment_array[i].capex[t]
-            #line_segment_opex_array[t]+=line_segment_array[i].opex[t]
-            #line_segment_environmental_restoration_cost_array[t]+=line_segment_array[i].environmental_restoration[t]     
+            line_segment_array[i].calculate_total_cost() 
             df_new=pd.DataFrame({'segment number': [i],
                                  'year':[t],
                                  'length':[line_segment_array[i].length],
