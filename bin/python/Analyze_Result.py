e78bbc3093af130bbab24d952aab6c35cb245302
Mahsa Arabi <marabi@umass.edu>
2021-09-15 18:52:20 -0400

    Minor Modification

--- bin/python/Analyze_Result.py
+++ bin/python/Analyze_Result.py
@@ -8,6 +8,10 @@ Created on Wed Sep 15 09:42:52 2021
 import Simulate
 
 
-df_output=Simulate.run_cost_simulation_statusQuo_strategy(40)
-df_analyze_result=df_output.groupby(level=[0])[['capex','opex','total infra','environmental restoration','non fatal','fatal','total safety','total cost']].sum()
-print (df_analyze_result)
+df_output_statusQuo=Simulate.run_cost_simulation_statusQuo_strategy(40)
+df_analyze_result_statusQuo=df_output_statusQuo.groupby(level=[0])[['capex','opex','total infra','environmental restoration','non fatal','fatal','total safety','total cost']].sum()
+print (df_analyze_result_statusQuo)
+
+df_output_under=Simulate.run_cost_simulation_under_after_lifespan_strategy(40)
+df_analyze_result_under=df_output_under.groupby(level=[0])[['capex','opex','total infra','environmental restoration','non fatal','fatal','total safety','total cost']].sum()
+print (df_analyze_result_under)
