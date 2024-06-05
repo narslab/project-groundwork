51fa259601085453ec8330657194beeb786e06d9
maharabi <87103328+maharabi@users.noreply.github.com>
2024-06-04 20:26:21 -0400

    Sensitivity with plf=0.03

--- bin/python/Network.py
+++ bin/python/Network.py
@@ -62,7 +62,7 @@ class Electric_model_inputs:
             #"Shrewsbury_tax_levy_2021": 85713912.0,
             "Shrewsbury_tax_levy_2021": 6873609623,#85713912.0/(12.47/1000) the tax levy is $85,713,912 and the tax rate is $12.47 per $1,000 of assessed value
             #"aesthetic_benefit_proportion":0.03,
-            "aesthetic_benefit_proportion":0.05,
+            "aesthetic_benefit_proportion":0.03,
             "inflation_rate_benefit":0,
             "shrewsbury_population":38325,
             "residential_percentage":0.8,
@@ -78,7 +78,7 @@ class Electric_model_inputs:
             "length_s": 0.711,
             "length_scale": 0.019,
             "length_loc": -0.004,
-            "alpha":0.2
+            "alpha":0.4
             }
     
     
@@ -134,7 +134,7 @@ class Broadband_model_inputs:
             # om_proportion_replcost= percentage of the overall replacement costs which equals to annual O&M expenses (OPEX) for each type of line
             # corridor_width= length of the corridor in feet needed for calculating environmental cost.
             # over_under_convertcost= replacement cost associated with replacing an overhead line with an underground line.
-            "aesthetic_benefit_proportion":0.05,
+            "aesthetic_benefit_proportion":0.03,
             #"Shrewsbury_tax_levy_2021": 85713912.0,
             "Shrewsbury_tax_levy_2021": 6873609623,#85713912.0/(12.47/1000) the tax levy is $85,713,912 and the tax rate is $12.47 per $1,000 of assessed value
             "inflation_rate_benefit":0,
@@ -168,7 +168,7 @@ class Broadband_model_inputs:
             "Total_Customers_Residential_Shrewsbury":6449,
             "Total_Customers_Commercial_Shrewsbury":838,
             "Total_Customers_Industry_Shrewsbury":189,
-            "alpha":0.2
+            "alpha":0.4
             }
         
     #defining a function to modify parameters for sensitivity anlysis based on percentage change
