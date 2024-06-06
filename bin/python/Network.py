a0aa690e34f529bd7acf4e1ee344289ee7059fa0
maharabi <87103328+maharabi@users.noreply.github.com>
2024-06-05 21:06:34 -0400

    Sensitivity with VLL_B,R=10

--- bin/python/Network.py
+++ bin/python/Network.py
@@ -158,7 +158,7 @@ class Broadband_model_inputs:
             "SAIDI":1.38,
             "outage_overhead":0.8,
             "outage_underground":0.2,
-            "USD_per_Customer_Hour_Interruption_Residential": 2,
+            "USD_per_Customer_Hour_Interruption_Residential": 10,
             "USD_per_Customer_Hour_Interruption_Commercial":205.70,
             "USD_per_Customer_Hour_Interruption_Industry":9600,
             #Number of Customers in Each Sector in Shrewsbury Municipal Electric (SELCO) in 2019
